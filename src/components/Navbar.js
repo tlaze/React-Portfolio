b70f25ed192c576236b2e87d4ddcf3d1b0cc98f8
tlaze <tomlazore@gmail.com>
2021-11-15 21:47:43 -0500

    Bootstrap added to navbar. Displays Name,projects,contact,and resume links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,20 @@
+import {Navbar, Container, Nav} from 'react-bootstrap'
+
 function navBar() {
     return(
-        <div>
-            <h1>navBar</h1>
-        </div>
+        <Navbar fixed="top" expand="sm" bg="dark" variant="dark">
+            <Container className="ms-1 container-fluid">
+            <Navbar.Brand href="#home">Tom Lazore</Navbar.Brand>
+            <Navbar.Toggle />
+                <Navbar.Collapse>
+                    <Nav>
+                        <Nav.Link className="mx-4" href="#projects">Projects</Nav.Link>
+                        <Nav.Link className="mx-4" href="#contact">Contact Me</Nav.Link>
+                        <Nav.Link className="mx-4" href="#resume">Resume</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
 
