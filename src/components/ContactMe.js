a2a74224edc2441d2420545deb4eecc1a40db009
tlaze <tomlazore@gmail.com>
2021-11-17 14:30:40 -0500

    layout for contact form updated. Doesn't function correctly yet

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,25 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { validateEmail } from '../utils/helpers';
+import {Form, Button } from 'react-bootstrap'
 
 function ContactMe() {
-    const styles = {
-        contactStyle: {
-            marginTop:'25px',
-            height:'100%',
-        }
+
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === 'email') {
+        setEmail(inputValue);
+    } 
+    else if (inputType === 'name') {
+        setName(inputValue);
     }
-    return(
-        <div className="container-fluid" style={styles.contactStyle}>
-            <div className="row">
-                <div className="col-md-12">
-                    <h1>Contact Me</h1>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-md-12">
-                </div>
-            </div>
+    else{
+      setMessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+
+    e.preventDefault();
+
+    if (!validateEmail(email) || !name || !message) {
+      setErrorMessage('Enter a Valid Email. Name and Message must be filled out.');
+      return;
+    }
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
+
+  const styles = {
+    contactStyle: {
+      marginTop:'25px',
+      height:'100%',
+    }
+  }
+  return(
+    <div className="container-fluid" style={styles.contactStyle}>
+      <div className="row">
+        <div className="col-md-12">
+          <h1>Contact Me</h1>
         </div>
-    )   
+      </div>
+      <div className="row">
+        <div className="col-md-8 offset-md-2 my-5">
+          <Form>
+            <Form.Group className="mb-3" >
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter Name" name={name} value={name} onChange={handleInputChange} />
+            </Form.Group>
+            <Form.Group className="mb-3" >
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" placeholder="name@example.com" name={email} value={email} onChange={handleInputChange} />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Message</Form.Label>
+              <Form.Control as="textarea" rows={3} name={message} value={message} onChange={handleInputChange} />
+            </Form.Group>
+            <Button className="offset-5" variant="light" type="submit">Submit
+            </Button>
+          </Form>
+        </div>
+      </div>
+    </div>
+  )   
 }
 
 export default ContactMe;
+
+
+
+{/* <form className="form my-5">
+<label for="email">Email</label>
+<input className="col-3 offset-2" value={email} name="email" onChange={handleInputChange} type="email" placeholder="Email" />
+<input className="col-3 offset-2" value={name} name="name" onChange={handleInputChange} type="text" placeholder="Full Name" />
+<input className="col-8 offset-2 my-4" value={message} name="message" onchange={handleInputChange} type="text" placeholder="Message" />
+<button className="offset-6" type="button" onClick={handleFormSubmit}>Submit</button>
+{errorMessage && (
+  <div>
+    <p className="error-text text-center">{errorMessage}</p>
+  </div>
+)}
+</form> */}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,22 +15,22 @@ function NavBar({ currentPage, handlePageChange }) {
         linkStyle:{
             marginRight: '25px',
             marginLeft: '25px',
+            fontSize: '16px',
         }
     }
     return(
         <Navbar className="navbar-fixed-top container-fluid" expand="md" style={styles.navStyle}>
-                <Navbar.Brand className="brand ms-1" style={styles.brandStyle}>Tom Lazore</Navbar.Brand>
-                <Navbar.Toggle className="me-1" />
-                <Navbar.Collapse className="justify-content-center">
-                    <Nav>
-                        <Nav.Link style={styles.linkStyle}  className={ currentPage === 'about' ? 'nav-link active' : 'nav-link' } href="#about" onClick={() => handlePageChange('about')}>About Me</Nav.Link>
-                        <Nav.Link style={styles.linkStyle}  className={currentPage === 'projects' ? 'nav=link active' : 'nav-link' } href="#projects" onClick={() => handlePageChange('projects')}>Projects</Nav.Link>
-                        <Nav.Link style={styles.linkStyle}  className={currentPage === 'contact' ? 'nav=link active' : 'nav-link' } href="#contact" onClick={() => handlePageChange('contact')}>Contact Me</Nav.Link>
-                        <Nav.Link style={styles.linkStyle}  href={myResume} download="Lazore_Thomas_Resume" rel="noreferrer">Resume</Nav.Link>            
-                    </Nav>
-                </Navbar.Collapse>
+            <Navbar.Brand className="brand ms-1" style={styles.brandStyle}>Tom Lazore</Navbar.Brand>
+            <Navbar.Toggle className="me-1" />
+            <Navbar.Collapse className="justify-content-center">
+                <Nav>
+                    <Nav.Link style={styles.linkStyle}  className={ currentPage === 'about' ? 'nav-link active' : 'nav-link' } href="#about" onClick={() => handlePageChange('about')}>About Me</Nav.Link>
+                    <Nav.Link style={styles.linkStyle}  className={currentPage === 'projects' ? 'nav=link active' : 'nav-link' } href="#projects" onClick={() => handlePageChange('projects')}>Projects</Nav.Link>
+                    <Nav.Link style={styles.linkStyle}  className={currentPage === 'contact' ? 'nav=link active' : 'nav-link' } href="#contact" onClick={() => handlePageChange('contact')}>Contact Me</Nav.Link>
+                    <Nav.Link style={styles.linkStyle}  href={myResume} download="Lazore_Thomas_Resume" rel="noreferrer">Resume</Nav.Link>            
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
     )
 }
-
 export default NavBar;
