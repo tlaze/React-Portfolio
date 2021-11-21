1f6efde9bdb0af03d3d789b2f9bea530129ae6ca
tlaze <tomlazore@gmail.com>
2021-11-21 14:24:56 -0500

    update to project name, added modals to phone and email. Lost info from after renaming but got it restored

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faPhoneSquareAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import '../styles/footer.css';
+import { Modal, Button } from 'react-bootstrap';
+
+
+function PhoneModal(props) {
+    return (
+      <Modal {...props} size="sm" aria-labelledby="phone-modal" centered>
+        <Modal.Header>
+          <Modal.Title id="phone-modal">
+            Cell Phone
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>315-778-6632</h4>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide} className="bg-primary">Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+}
+
+
+function EmailModal(props) {
+    return (
+      <Modal {...props} size="sm" aria-labelledby="email-modal" centered>
+        <Modal.Header>
+          <Modal.Title id="email-modal">
+            Email
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>tomlazore@gmail.com</h4>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide} className="bg-primary">Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+}
+
 
 function Footer() {
-
+    const [phoneModalShow, setPhoneModalShow] = useState(false);
+    const [emailModalShow, setEmailModalShow] = useState(false);
     return(
+
         <footer className="footerStyle">
             <a href='https://github.com/tlaze' target='blank' className="footerIcons"><FontAwesomeIcon icon={faGithubSquare} size="3x" /></a>
             <a href='https://www.linkedin.com/in/tlaze' target='blank' className="footerIcons"><FontAwesomeIcon icon={faLinkedin} size="3x" /></a>
-            <button data-toggle="modal" className="footerIcons" ><FontAwesomeIcon icon={faPhoneSquareAlt} size="3x" /></button>
-            <button className="footerIcons"><FontAwesomeIcon icon={faEnvelope} size="3x" /></button>
-        
+            <Button className="footerIcons" onClick={() => setPhoneModalShow(true)}><FontAwesomeIcon icon={faPhoneSquareAlt} size="3x"/></Button>
+            <Button className="footerIcons" onClick={() => setEmailModalShow(true)}><FontAwesomeIcon icon={faEnvelope} size="3x"/></Button>
+            <PhoneModal show={phoneModalShow} onHide={() => setPhoneModalShow(false)} />
+            <EmailModal show={emailModalShow} onHide={() => setEmailModalShow(false)} />
         </footer>
     )
 }
