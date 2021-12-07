cd26f36c01cd2b7bde118e29b296fbaef482580f
tlaze <tomlazore@gmail.com>
2021-12-07 18:13:23 -0500

    updated contact me form layout

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -4,43 +4,31 @@ import {Form, Button } from 'react-bootstrap'
 
 function ContactMe() {
 
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
-  const [message, setMessage] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [status, setStatus] = useState("Submit");
 
-  const handleInputChange = (e) => {
-    // Getting the value and name of the input which triggered the change
-    const { target } = e;
-    const inputType = target.name;
-    const inputValue = target.value;
-
-    if (inputType === 'email') {
-        setEmail(inputValue);
-    } 
-    else if (inputType === 'name') {
-        setName(inputValue);
-    }
-    else{
-      setMessage(inputValue);
-    }
-  };
-
-  const handleFormSubmit = (e) => {
-
+  const handleSubmit = async(e) => {
     e.preventDefault();
+    setStatus("Sending..");
 
-    if (!validateEmail(email)) {
-      setErrorMessage('Enter a Valid Email. Name and Message must be filled out.');
-      return;
-    }
-    if(!email || !name || !message){
-      setErrorMessage('Inputs must be filled out!');
-      return;
-    }
-    setName('');
-    setEmail('');
-    setMessage('');
+    const {name, email, message} = e.target.elements;
+
+    let formInput = {
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    };
+
+    let response = await fetch("http://localhost:3000/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(formInput),
+    });
+
+    setStatus("Submit");
+    let result = await response.json();
+    alert(result.status);
   };
 
   const styles = {
@@ -58,45 +46,21 @@ function ContactMe() {
       </div>
       <div className="row">
         <div className="col-md-8 offset-md-2 my-5">
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" >
-              <Form.Label>Name</Form.Label>
-              <Form.Control
-               type="text" 
-               name="name" 
-               placeholder="Enter Name" 
-               value={name} 
-               onChange={handleInputChange} 
-               />
+              <Form.Label htmlFor="name">Name</Form.Label>
+              <Form.Control type="text" id="name" required placeholder="Enter Your Full Name" />
             </Form.Group>
             <Form.Group className="mb-3" >
-              <Form.Label>Email</Form.Label>
-              <Form.Control
-               type="email"
-               name="email" 
-               placeholder="name@example.com" 
-               value={email} 
-               onChange={handleInputChange}
-               />
+              <Form.Label htmlFor="email">Email</Form.Label>
+              <Form.Control type="email" id="email" required placeholder="email@example.com" />
             </Form.Group>
             <Form.Group className="mb-3">
-              <Form.Label>Message</Form.Label>
-              <Form.Control
-               as="textarea"
-               name="message" 
-               rows={4} 
-               value={message} 
-               onChange={handleInputChange}
-                />
+              <Form.Label htmlFor="message">Message</Form.Label>
+              <Form.Control as="textarea" id="message" required rows={4} />
             </Form.Group>
-            <Button className="col-2 offset-5 bg-light" variant="light" type="submit" onChange={handleFormSubmit}>Submit
-            </Button>
+            <Button className="col-2 offset-5 bg-light" variant="light" type="submit" >{status}</Button>
           </Form>
-          {errorMessage && (
-            <div>
-              <p className="error-text text-center">{errorMessage}</p>
-            </div>
-          )}
         </div>
       </div>
     </div>
